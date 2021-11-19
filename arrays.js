e60a8225856f4a0b675333a6d7e139e993800157
Nydia Medina <nvmedina29@gmail.com>
2021-11-18 19:55:16 -0600

    Completing Arrays section

--- arrays.js
+++ arrays.js
@@ -5,10 +5,15 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+let colorCopy = faveColors.slice(0, 3);
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+
+colorCopy.push('pink');
+
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -20,6 +25,7 @@ const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
 
+let middleNums = numbers.slice(1, 4);
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -35,3 +41,15 @@ const numbers = [1, 2, 3, 4, 5]
 */
 
 // CODE HERE
+
+function bigOrSmall(arr) {
+  let answers = [];
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] > 100) {
+      answers.push('big');
+    } else {
+      answers.push('small');
+    }
+  }
+  return answers;
+}
