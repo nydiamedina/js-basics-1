b7ac0d4d4b7491c9a339bfbd331fae1ce13ce26e
Nydia Medina <nvmedina29@gmail.com>
2021-11-18 19:37:08 -0600

    Completing the Software Development Lifecycle section

--- sldc.js
+++ sldc.js
@@ -5,15 +5,22 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+
+let planning = 'This step is about defining an outline for the upcoming project, including resources, cost estimations, and scheduling.';
+let analysisOfRequirements = 'This step is about identifying and defining the requirements from the client, and also defining the technology stack.';
+let design = 'This step is about building the architecture of the project and prototyping how the software integrations will work.';
+let implementation = 'This step encompasses the development process of the project, which includes coding and building the application.';
+let testingAndIntegration = 'During this step, the software is assessed for errors in order to document and fix bugs.';
+let maintain = 'This step is about making modifications and upgrades based on end-user needs or changes in the requirements.';
