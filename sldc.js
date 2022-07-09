4e9fb896f2100850c8b53f8084fdc280bef5f920
Nydia Medina <nvmedina29@gmail.com>
2022-07-09 14:56:30 -0600

    Making definition's adjustments in sldc.js file.

--- sldc.js
+++ sldc.js
@@ -18,9 +18,9 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
 
-let planning = 'This step is about defining an outline for the upcoming project, including resources, cost estimations, and scheduling.';
-let analysisOfRequirements = 'This step is about identifying and defining the requirements from the client, and also defining the technology stack.';
-let design = 'This step is about building the architecture of the project and prototyping how the software integrations will work.';
+let planning = 'During this step, an outline of the upcoming project is defined, which includes resources, cost estimations, and scheduling.';
+let analysisOfRequirements = 'This step is about identifying and defining the client\'s requirements for the new project, and defining the technology stack as well.';
+let design = 'This step is about designing and building the architecture of the project and prototyping how the different software integrations will work.';
 let implementation = 'This step encompasses the development process of the project, which includes coding and building the application.';
 let testingAndIntegration = 'During this step, the software is assessed for errors in order to document and fix bugs.';
 let maintain = 'This step is about making modifications and upgrades based on end-user needs or changes in the requirements.';
